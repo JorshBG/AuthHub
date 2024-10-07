bbe8dad124d7daef79b70d52c33f3585f466da8d
JorshBG <jordiyair29@gmail.com>
2024-10-06 19:18:22 -0600

    Add handlers for header and authhub exceptions

--- src/main/java/com/jorshbg/authhub/system/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/jorshbg/authhub/system/exceptions/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.jorshbg.authhub.system.exceptions;
 import com.jorshbg.authhub.utils.responses.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.ResponseStatusException;
@@ -22,6 +23,26 @@ public class GlobalExceptionHandler {
                 "Warning",
                 LocalDateTime.now(ZoneId.systemDefault())
         ), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<ErrorResponse> handleMissingRequestHeaderException(final MissingRequestHeaderException e) {
+        return new ResponseEntity<>(new ErrorResponse(
+                "Header Not Found or Missing Value",
+                e.getLocalizedMessage(),
+                "Warning",
+                LocalDateTime.now(ZoneId.systemDefault())
+        ), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AuthHubException.class)
+    public ResponseEntity<ErrorResponse> handleAuthHubException(final AuthHubException e) {
+        return new ResponseEntity<>(new ErrorResponse(
+                null,
+                e.getReason(),
+                e.getStatusCode().value() >= 500? "Error" : "Warning",
+                LocalDateTime.now(ZoneId.systemDefault())
+        ), e.getStatusCode());
     }
 
     @ExceptionHandler(ResponseStatusException.class)
