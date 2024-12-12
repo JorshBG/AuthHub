a850cac72440eeadd45aaab5421e0f10b07461cf
JorshBG <jordiyair29@gmail.com>
2024-12-12 15:16:08 -0600

    Change type of data

--- src/main/java/com/jorshbg/authhub/utils/responses/PaginatedResponse.java
+++ src/main/java/com/jorshbg/authhub/utils/responses/PaginatedResponse.java
@@ -1,10 +1,12 @@
 package com.jorshbg.authhub.utils.responses;
 
+import java.util.List;
+
 /**
  * Paginated response record to response to the client
  * @param data Array data
  * @param metadata Information about paginated object
  * @param <T> Type of the array data
  */
-public record PaginatedResponse<T>(T[] data, MetadataResponse metadata) {}
+public record PaginatedResponse<T>(List<T> data, MetadataResponse metadata) {}
 
